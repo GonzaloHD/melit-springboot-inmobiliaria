27711c97ab578e70464b7cba288ce723459c28c1
Letspr <lparrondo@gmail.com>
2023-12-14 17:17:54 +0100

    relacion cliente-demanda

--- src/main/java/es/melit/melitspringbootinmobiliaria/entities/Demanda.java
+++ src/main/java/es/melit/melitspringbootinmobiliaria/entities/Demanda.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @SuppressWarnings("serial")
 @Entity
@@ -17,6 +19,11 @@ public class Demanda implements Serializable {
 	@Column(name = "id_demanda")
 	private Integer idDemanda;
 	private String descripcion;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_cliente", nullable = false)
+	private Cliente cliente;
+	
 	public Integer getIdDemanda() {
 		return idDemanda;
 	}
