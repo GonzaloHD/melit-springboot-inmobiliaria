0ecddb5eb7f396177dbc92168b37e53646b87c43
DESKTOP-H005B3Q\melit <gonzalohernandezduque@gmail.com>
2023-12-26 17:57:52 +0100

    ajustes

--- src/main/java/es/melit/melitspringbootinmobiliaria/controllers/InmuebleController.java
+++ src/main/java/es/melit/melitspringbootinmobiliaria/controllers/InmuebleController.java
@@ -3,6 +3,7 @@ package es.melit.melitspringbootinmobiliaria.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @Tag(name = "Gestion de inmuebles", description = "CRUD y funcionalidad de inmuebles")
 @RequestMapping(path = "/inmuebles")
+@CrossOrigin(origins = "http://localhost:4200")
 public class InmuebleController {
 	
 	private InmuebleService inmuebleService;
